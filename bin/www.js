697cf0b55e4dea1ab6e12d86d9409f5237020801
matw974 <mathieu.wautier@gmail.com>
2016-02-25 12:21:54 +0000

    Fixed #1

---
+++ bin/www.js
@@ -0,0 +1,11 @@
+var app = require("../server");
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(err){
+    if(err){
+        console.dir(err);
+    }else{
+        console.log("The server is listening");
+    }
+});
--- server.js
+++ server.js
@@ -6,12 +6,4 @@ app.get("/", function(req, resp, next){
   resp.send("Node at Idweaver!");  
 })
 
-var port = process.env.PORT || 3000;
-
-app.listen(port, function(err){
-    if(err){
-        console.dir(err);
-    }else{
-        console.log("The server is listening");
-    }
-})
+module.exports = app;
