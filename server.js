0bce22785e1fcf1fd43820922231e1f2f8be7e4b
matw974 <mathieu.wautier@gmail.com>
2016-03-03 12:24:14 +0000

    add .

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.engine('hbs', exphbs({
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs'
 }));
-app.set('view engine', 'hbs');
+app.set('view engine', '.hbs');
 
 app.get("/", function(req, res, next){
   res.render('home');  
